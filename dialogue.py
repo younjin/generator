3a752c68c6e0260ff6c6f90d5621fd21e1eb26a3
Jinny <younjin@users.noreply.github.com>
2023-03-02 22:45:47 +0100

    Bobo makes an appearance

--- dialogue.py
+++ dialogue.py
@@ -1,2 +1,12 @@
-x = input("Hello! What's your name?")
-print("Hello, " + x + "!")
+def greetings(name):
+
+    guess = input("Hello! Is your name {}? ".format(name))
+    
+    if guess.lower() == "yes":
+        print("Hello, {}!".format(name))
+    
+    else:
+        actual = input("Okay, what is your name then? ")
+        print("Hello, {}!".format(actual))
+
+greetings("Bobo")
